a563a0570f613e5781c504b30f21a466420c0850
Danilov_Nikolai <dan1k91@mail.ru>
2024-01-11 19:21:04 +0300

    icons_fixed

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import successIcon from "../assets/success.svg";
 
 export const Success = ({ count }) => {
   return (
     <div className="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src={successIcon} alt="Success" />
       <h3>Успешно!</h3>
       <p>{count} пользователям отправлено приглашение.</p>
       <button
